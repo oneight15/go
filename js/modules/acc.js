1fb8cb473b74df83325cd89b2ff653b1bc79f991
oneight15 <oneight1508@gmail.com>
2023-09-06 17:39:53 +0500

    feat: add acc

---
+++ js/modules/acc.js
@@ -0,0 +1,19 @@
+const items = document.querySelectorAll('.faq__item');
+const btns = document.querySelectorAll('.faq__btn');
+const answerWrappers = document.querySelectorAll('.faq__answer-wrapper');
+
+btns.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    for (let i = 0; i < items.length; i += 1) {
+      if (index === i) {
+        answerWrappers[i].style.height =
+          items[i].classList.contains('item_active') ?
+          '' : `214px`;
+        items[i].classList.toggle('item_active');
+      } else {
+        items[i].classList.remove('item_active');
+        answerWrappers[i].style.height = '';
+      }
+    }
+  });
+});
