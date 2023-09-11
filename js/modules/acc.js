17cc58b87f5c291faef605d0499cdb69216b3e2e
oneight15 <oneight1508@gmail.com>
2023-09-11 16:00:46 +0500

    fix: correct acc.js

--- js/modules/acc.js
+++ js/modules/acc.js
@@ -1,5 +1,6 @@
 const items = document.querySelectorAll('.faq__item');
 const answerWrappers = document.querySelectorAll('.faq__answer-wrapper');
+const paths = document.querySelectorAll('.path');
 
 items.forEach((btn, index) => {
   btn.addEventListener('click', () => {
@@ -7,8 +8,9 @@ items.forEach((btn, index) => {
       if (index === i) {
         answerWrappers[i].style.height =
           items[i].classList.contains('item_active') ?
-          '' : `250px`;
+          '' : `214px`;
         items[i].classList.toggle('item_active');
+        paths[i].style.stroke = 'transparent';
       } else {
         items[i].classList.remove('item_active');
         answerWrappers[i].style.height = '';
