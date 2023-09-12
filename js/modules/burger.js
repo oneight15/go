d5c8e0f372e28fddc084d0ca1ba7a5073178b43a
oneight15 <oneight1508@gmail.com>
2023-09-12 23:02:48 +0500

    feat: add tablet burger menu

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -1,5 +1,14 @@
 const burger = document.querySelector('.burger');
+const nav = document.querySelector('.navigation');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger_active');
+  nav.classList.toggle('navigation_active');
+});
+
+nav.addEventListener('click', e => {
+  if (e.target.classList.contains('navigation__link')) {
+    burger.classList.remove('burger_active');
+    nav.classList.remove('navigation_active');
+  }
 });
