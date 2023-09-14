bd3b9a7c2f82c563a897e802c84e9361ab542c8e
oneight15 <oneight1508@gmail.com>
2023-09-14 15:38:55 +0500

    fix: correct adaptive, add call-btn in burger menu on mobile

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -1,14 +1,17 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.navigation');
+const headWrap = document.querySelector('.header-wrapper');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger_active');
   nav.classList.toggle('navigation_active');
+  headWrap.classList.toggle('header-wrapper_active');
 });
 
 nav.addEventListener('click', e => {
   if (e.target.classList.contains('navigation__link')) {
     burger.classList.remove('burger_active');
     nav.classList.remove('navigation_active');
+    headWrap.classList.remove('header-wrapper_active');
   }
 });
