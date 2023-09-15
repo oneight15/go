6d8b31dd24d7c8d2c48631fed4378e93fbe92f1c
oneight15 <oneight1508@gmail.com>
2023-09-15 17:34:46 +0500

    feat: add open burger anim

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -1,17 +1,46 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.navigation');
 const headWrap = document.querySelector('.header-wrapper');
+const navList = document.querySelector('.navigation__list');
+const duration = 300;
+let startTime = NaN;
+
+const openBurger = (timestamp) => {
+  startTime ||= timestamp;
+  const progress = (timestamp - startTime) / duration;
+  navList.style.opacity = progress;
+
+  if (progress < 1) {
+    requestAnimationFrame(openBurger);
+  }
+};
+
+const closeBurger = (timestamp) => {
+  startTime ||= timestamp;
+  const progress = (timestamp - startTime) / duration;
+  navList.style.opacity = 1 - progress;
+
+  if (progress < 1) {
+    requestAnimationFrame(closeBurger);
+  } else {
+    burger.classList.remove('burger_active');
+    nav.classList.remove('navigation_active');
+    headWrap.classList.remove('header-wrapper_active');
+    startTime = NaN;
+  }
+};
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger_active');
   nav.classList.toggle('navigation_active');
   headWrap.classList.toggle('header-wrapper_active');
+  requestAnimationFrame(openBurger);
+  startTime = NaN;
 });
 
-nav.addEventListener('click', e => {
+document.addEventListener('click', e => {
   if (e.target.classList.contains('navigation__link')) {
-    burger.classList.remove('burger_active');
-    nav.classList.remove('navigation_active');
-    headWrap.classList.remove('header-wrapper_active');
+    requestAnimationFrame(closeBurger);
+    startTime = NaN;
   }
 });
