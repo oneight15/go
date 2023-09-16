cf186d2f7712e3ed336a06ae6ee005a595976160
oneight15 <oneight1508@gmail.com>
2023-09-16 21:21:09 +0500

    feat: add anim close burger

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -9,6 +9,7 @@ const openBurger = (timestamp) => {
   startTime ||= timestamp;
   const progress = (timestamp - startTime) / duration;
   navList.style.opacity = progress;
+  // headWrap.style.opacity = progress;
 
   if (progress < 1) {
     requestAnimationFrame(openBurger);
@@ -22,20 +23,27 @@ const closeBurger = (timestamp) => {
 
   if (progress < 1) {
     requestAnimationFrame(closeBurger);
-  } else {
+  }
+
+  if (navList.style.opacity <= 0) {
     burger.classList.remove('burger_active');
     nav.classList.remove('navigation_active');
     headWrap.classList.remove('header-wrapper_active');
-    startTime = NaN;
   }
 };
 
 burger.addEventListener('click', () => {
-  burger.classList.toggle('burger_active');
-  nav.classList.toggle('navigation_active');
-  headWrap.classList.toggle('header-wrapper_active');
-  requestAnimationFrame(openBurger);
-  startTime = NaN;
+  if (burger.classList.contains('burger_active')) {
+    // burger.classList.remove('burger_active');
+    startTime = NaN;
+    requestAnimationFrame(closeBurger);
+  } else {
+    burger.classList.add('burger_active');
+    nav.classList.add('navigation_active');
+    headWrap.classList.add('header-wrapper_active');
+    requestAnimationFrame(openBurger);
+    startTime = NaN;
+  }
 });
 
 document.addEventListener('click', e => {
